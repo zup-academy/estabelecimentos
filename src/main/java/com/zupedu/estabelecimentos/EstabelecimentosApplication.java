dfc48e02eb337efa01b6b9bd622d27db70a22fca
paula.santana <paula.santana@zup.com.br>
2022-02-25 19:16:04 -0300

    estrutura do projeto

---
+++ src/main/java/com/zupedu/estabelecimentos/EstabelecimentosApplication.java
@@ -0,0 +1,13 @@
+package com.zupedu.estabelecimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstabelecimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstabelecimentosApplication.class, args);
+	}
+
+}
